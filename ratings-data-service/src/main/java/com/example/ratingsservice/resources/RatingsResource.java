fa2c621c1d68381b196301f22a93c224a3e19610
Esraa Aboshady <78180296+ess2024@users.noreply.github.com>
2024-03-12 05:52:38 +0200

    Update RatingsResource.java

--- ratings-data-service/src/main/java/com/example/ratingsservice/resources/RatingsResource.java
+++ ratings-data-service/src/main/java/com/example/ratingsservice/resources/RatingsResource.java
@@ -1,11 +1,16 @@
 package com.example.ratingsservice.resources;
-
 import com.example.ratingsservice.models.Rating;
 import com.example.ratingsservice.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,12 +18,23 @@ import java.util.List;
 @RequestMapping("/ratings")
 public class RatingsResource {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    private class RatingMapper implements RowMapper<Rating> {
+        public Rating mapRow(ResultSet result, int rowNumber) throws SQLException {
+            Rating rating = new Rating();
+            rating.setMovieId(result.getString("movie_id"));
+            rating.setRating(result.getInt("rating"));
+            return rating;
+        }
+    }
     @RequestMapping("/{userId}")
     public UserRating getRatingsOfUser(@PathVariable String userId) {
-        List<Rating> ratings = Arrays.asList(
-                new Rating("550", 4)
-        );
+
+        String sql = "SELECT movie_id, rating FROM user_ratings WHERE user_id = ?";
+        List<Rating> ratings = jdbcTemplate.query(sql, new Object[]{userId}, new RatingMapper());
 
         return new UserRating(ratings);
     }
 }
+
